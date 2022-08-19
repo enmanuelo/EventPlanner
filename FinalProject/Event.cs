7d86637ad02b120cf510a6a9cda8c6c0e4cd6372
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-08-19 13:09:33 -0400

    Added new methods for getting start date, start time, end date, end time, and event genre

--- FinalProject/Event.cs
+++ FinalProject/Event.cs
@@ -6,7 +6,7 @@ namespace FinalProject
 		public Event()
 		{
 		}
-        public string AcquireDate(string someDate)
+        public string ArrivalDate(string someDate)
         {            
             var startDate = someDate;
 
@@ -63,7 +63,7 @@ namespace FinalProject
 
             return startDate;
         }
-        public string AcquireDate()
+        public string ArrivalDate()
         {
             var startDate = "";
 
@@ -71,7 +71,7 @@ namespace FinalProject
 
             do
             {
-                Console.WriteLine("When will you be visiting? (Enter using YYYY-MM-DD format");
+                Console.WriteLine("When will you be visiting? (Enter using YYYY-MM-DD format)");
                 startDate = Console.ReadLine();
 
                 if (int.Parse(startDate.Substring(0, 4)) < 2022)
@@ -114,17 +114,193 @@ namespace FinalProject
 
             return startDate;
         }
+        public string DepartureDate()
+        {
+            var endDate = "";
 
+            var valid = false;
+
+            do
+            {
+                Console.WriteLine("When will you be leaving? (Enter using YYYY-MM-DD format)");
+                endDate = Console.ReadLine();
+
+                if (int.Parse(endDate.Substring(0, 4)) < 2022)
+                {
+                    Console.WriteLine("Invalid year! Must pick a future date");
+                    valid = true;
+                }
+                else if (int.Parse(endDate.Substring(5, 2)) < 1 || int.Parse(endDate.Substring(5, 2)) > 12)
+                {
+                    Console.WriteLine("Invalid month! Must pick an existing month between 01-12");
+                    valid = true;
+                }
+                else if ((int.Parse(endDate.Substring(5, 2)) == 2) && (int.Parse(endDate.Substring(8, 2)) < 1 || int.Parse(endDate.Substring(8, 2)) > 28))
+                {
+                    Console.WriteLine("Invalid day! Must choose between 01-28 for the month of February");
+                    valid = true;
+                }
+                else if ((int.Parse(endDate.Substring(5, 2)) == 4 || int.Parse(endDate.Substring(5, 2)) == 6 || int.Parse(endDate.Substring(5, 2)) == 9 || int.Parse(endDate.Substring(5, 2)) == 11) && (int.Parse(endDate.Substring(8, 2)) < 1 || int.Parse(endDate.Substring(8, 2)) > 30))
+                {
+                    Console.WriteLine("Invalid day! Must choose day between 01-30 for this month");
+                    valid = true;
+
+                }
+                else if ((int.Parse(endDate.Substring(5, 2)) == 1 || int.Parse(endDate.Substring(5, 2)) == 3 || int.Parse(endDate.Substring(5, 2)) == 5 || int.Parse(endDate.Substring(5, 2)) == 7 || int.Parse(endDate.Substring(5, 2)) == 8 || int.Parse(endDate.Substring(5, 2)) == 10 || int.Parse(endDate.Substring(5, 2)) == 12) && (int.Parse(endDate.Substring(8, 2)) < 1 || int.Parse(endDate.Substring(8, 2)) > 31))
+                {
+                    Console.WriteLine("Invalid day! Must choose day between 01-31 for this month");
+                    valid = true;
+                }
+                else if (endDate[4] != '-' || endDate[7] != '-')
+                {
+                    Console.WriteLine("Invalid input! Must use '-' to separate year, month and date. Please check format!");
+                    valid = true;
+                }
+                else
+                {
+                    valid = false;
+                }
+            } while (valid == true);
+
+
+            return endDate;
+        }
         public string AcquireCity()
         {
             Console.WriteLine("What city will you be visiting?");
-            return Console.ReadLine();
+            var city = Console.ReadLine();
+            return city;
         }
-        public string AcquireTime()
+        public string StartTime()
         {
-            Console.WriteLine("What is the soonest time you will be available for the event? (Use military time. Example: 3:30 pm would be entered as 15:30");
-            var startTime = Console.ReadLine();
-            return startTime;
+            var time = "";
+            string hour;            
+            var valid = false;
+
+            do
+            {
+                Console.WriteLine("What is the soonest time you will be available for the event? (Use HH:MM_M format, where 'HH' is the hour, 'MM' is the minutes, and '_M' is either AM or PM.");
+                time = Console.ReadLine();
+
+                if (int.Parse(time.Substring(0, 2)) < 1 || int.Parse(time.Substring(0, 2)) > 12)
+                {
+                    Console.WriteLine("Invalid hour! Please choose an hour between 01 and 12.");
+                    valid = true;
+                }
+                else if (int.Parse(time.Substring(3, 2)) < 1 || int.Parse(time.Substring(3, 2)) > 59)
+                {
+                    Console.WriteLine("Invalid minute! Please choose a minute between 01 and 59.");
+                    valid = true;
+                }
+                else if (time[2] != ':')
+                {
+                    Console.WriteLine("Invalid input! Please use ':' to separate hours and minutes.");
+                    valid = true;
+                }
+                else if (time.Length <= 5)
+                {
+                    Console.WriteLine("Invalid input! Must indicate whether AM or PM");
+                    valid = true;
+                }
+                else if (!(time.Substring(5).ToLower().Equals("am") || time.Substring(5).ToLower().Equals("pm")))
+                {
+                    Console.WriteLine($"Invalid input! Enter either 'AM' or 'PM' after time. You entered {time.Substring(5)}");
+                    valid = true;
+                }
+                else
+                {
+                    valid = false;
+                }
+            } while (valid == true);
+            
+            if (time.Substring(5, 2).ToUpper() == "PM")
+            {
+                hour = (int.Parse(time.Substring(0, 2)) + 12).ToString();
+            }
+            else
+            {
+                hour = time.Substring(0, 2);
+            }
+
+            var minutes = time.Substring(3, 2);
+            time = hour + minutes + ":00";
+
+            return time;
+        }
+        public string EndTime()
+        {
+            var time = "";
+            string hour;
+            var valid = false;
+
+            do
+            {
+                Console.WriteLine("What is the latest time you can attend event? (Use HH:MM_M format, where 'HH' is the hour, 'MM' is the minutes, and '_M' is either AM or PM.");
+                time = Console.ReadLine();
+
+                if (int.Parse(time.Substring(0, 2)) < 1 || int.Parse(time.Substring(0, 2)) > 12)
+                {
+                    Console.WriteLine("Invalid hour! Please choose an hour between 01 and 12.");
+                    valid = true;
+                }
+                else if (int.Parse(time.Substring(3, 2)) < 1 || int.Parse(time.Substring(3, 2)) > 59)
+                {
+                    Console.WriteLine("Invalid minute! Please choose a minute between 01 and 59.");
+                    valid = true;
+                }
+                else if (time[2] != ':')
+                {
+                    Console.WriteLine("Invalid input! Please use ':' to separate hours and minutes.");
+                    valid = true;
+                }
+                else if (time.Length <= 5)
+                {
+                    Console.WriteLine("Invalid input! Must indicate whether AM or PM");
+                    valid = true;
+                }
+                else if (!(time.Substring(5).ToLower().Equals("am") || time.Substring(5).ToLower().Equals("pm")))
+                {
+                    Console.WriteLine($"Invalid input! Enter either 'AM' or 'PM' after time. You entered {time.Substring(5)}");
+                    valid = true;
+                }
+                else
+                {
+                    valid = false;
+                }
+            } while (valid == true);
+
+            if (time.Substring(5, 2).ToUpper() == "PM")
+            {
+                hour = (int.Parse(time.Substring(0, 2)) + 12).ToString();
+            }
+            else
+            {
+                hour = time.Substring(0, 2);
+            }
+
+            var minutes = time.Substring(3, 2);
+            time = hour + minutes + ":00";
+
+            return time;
+        }
+        public string EventGenre()
+        {
+            Console.WriteLine("What type of event are you interested in attending? Sports, Music, or Miscellaneous");
+            var eventType = Console.ReadLine().ToLower();
+            if (eventType == "sports")
+            {
+                Console.WriteLine("Which sport? ");
+            }
+            else if (eventType == "music")
+            {
+                Console.WriteLine("Which musical genre? ");
+            }
+            else
+            {
+                Console.WriteLine("Which genre? ");
+            }
+            var genre = Console.ReadLine().ToLower();
+            return genre;
         }
     }
 }
--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -7,35 +7,28 @@ var key = Console.ReadLine();
 
 var eventO = new Event();
 
-/* Ask client for the city where they will be traveling to */
-var city = eventO.AcquireCity;
+/* Ask client for travel destination */
+var city = eventO.AcquireCity();
 
-/* Ask client for the date they will be traveling */
-var startDate = eventO.AcquireDate();
+/* Ask client for date of arrival */
+var startDate = eventO.ArrivalDate();
 
-/* Ask client for the time they will be available for event */
-Console.WriteLine("What is the soonest time you will be available for the event? (Use military time. Example: 3:30 pm would be entered as 15:30");
-var startTime = Console.ReadLine();
-var startDateTime = $"{startDate}T{startTime}:00Z";
+/* Ask client for time of arrival */
+var startTime = eventO.StartTime();
+
+/* Ask client for date of departure */
+var endDate = eventO.DepartureDate();
+
+/* Ask client for time of departure */
+var endTime = eventO.EndTime;
 
 /* Ask client what type of event */
-Console.WriteLine("What type of event are you interested in attending? Sports, Music, or Miscellaneous");
-var eventType = Console.ReadLine().ToLower();
-if (eventType == "sports")
-{
-    Console.WriteLine("Which sport? ");
-}
-else if (eventType == "music")
-{
-    Console.WriteLine("Which musical genre? ");
-}
-else
-{
-    Console.WriteLine("Which genre? ");
-}
-var genre = Console.ReadLine().ToLower();
+var genre = eventO.EventGenre();
 
-var eventURL = $"https://app.ticketmaster.com/discovery/v2/events.json?city={city}&startDateTime{startDateTime}&apikey={key}";
+var startDateTime = $"{startDate}T{startTime}Z";
+var endDateTime = $"{endDate}T{endTime}Z";
+
+var eventURL = $"https://app.ticketmaster.com/discovery/v2/events.json?city={city}&startDateTime{startDateTime}&endDateTime={endDateTime}&apikey={key}";
 var response = client.GetStringAsync(eventURL).Result;
 
 JObject jsonResponse = JObject.Parse(response);
